f2e0e5bbd80995cfccd2a538b70c91caa80b2c2e
Builder <builder@example.com>
2019-05-11 20:41:59 +0300

    update to 0.3.5.8

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 apt-get update && apt-get install -y nsis wget unzip
 
-wget https://www.torproject.org/dist/torbrowser/8.0/tor-win32-0.3.3.9.zip
+wget https://dist.torproject.org/torbrowser/8.0.9/tor-win32-0.3.5.8.zip
 unzip -d Tor tor-win32-*.zip && rm tor-win32-*.zip
 makensis -DTOR_PROXY_VERSION=$TOR_PROXY_VERSION tor-proxy.nsi
 rm -rf Tor && mkdir dist && mv *.exe dist/
--- version_env.sh
+++ version_env.sh
@@ -1,1 +1,1 @@
-export TOR_PROXY_VERSION=0.3.3.9
+export TOR_PROXY_VERSION=0.3.5.8
